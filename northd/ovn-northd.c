3d4f5e178dc9954d16b200e9dce8e72c76c5ffd1
Ilya Maximets <i.maximets@ovn.org>
2022-09-01 18:18:58 +0200

    northd: Accumulate more database updates before processsing.
    northd doesn't process changes incrementally, so it makes sense
    to accumulate more database updates and process them in bulk,
    so we can cover all of them in a single recompute.
    
    Calling ovsdb_idl_run() as long as something changes.  500 ms
    seems like a reasonable hard limit to avoid spinning for too
    long if there are too many constant changes.
    
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- northd/ovn-northd.c
+++ northd/ovn-northd.c
@@ -685,6 +685,30 @@ get_probe_interval(const char *db, const struct nbrec_nb_global *nb)
     return interval;
 }
 
+static struct ovsdb_idl_txn *
+run_idl_loop(struct ovsdb_idl_loop *idl_loop, const char *name)
+{
+    unsigned long long duration, start = time_msec();
+    unsigned int seqno = UINT_MAX;
+    int n = 0;
+
+    /* Accumulate database changes as long as there are some,
+     * but no longer than half a second. */
+    while (seqno != ovsdb_idl_get_seqno(idl_loop->idl)
+           && time_msec() - start < 500) {
+        seqno = ovsdb_idl_get_seqno(idl_loop->idl);
+        ovsdb_idl_run(idl_loop->idl);
+        n++;
+    }
+
+    duration = time_msec() - start;
+    if (n > 1 || duration > 100) {
+        VLOG_INFO("run_idl_loop: %s: %d iterations in %lld ms",
+                  name, n, duration);
+    }
+    return ovsdb_idl_loop_run(idl_loop);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -821,8 +845,8 @@ main(int argc, char *argv[])
                 ovsdb_idl_set_lock(ovnsb_idl_loop.idl, "ovn_northd");
             }
 
-            struct ovsdb_idl_txn *ovnnb_txn =
-                        ovsdb_idl_loop_run(&ovnnb_idl_loop);
+            struct ovsdb_idl_txn *ovnnb_txn = run_idl_loop(&ovnnb_idl_loop,
+                                                           "OVN_Northbound");
             unsigned int new_ovnnb_cond_seqno =
                         ovsdb_idl_get_condition_seqno(ovnnb_idl_loop.idl);
             if (new_ovnnb_cond_seqno != ovnnb_cond_seqno) {
@@ -833,8 +857,8 @@ main(int argc, char *argv[])
                 ovnnb_cond_seqno = new_ovnnb_cond_seqno;
             }
 
-            struct ovsdb_idl_txn *ovnsb_txn =
-                        ovsdb_idl_loop_run(&ovnsb_idl_loop);
+            struct ovsdb_idl_txn *ovnsb_txn = run_idl_loop(&ovnsb_idl_loop,
+                                                           "OVN_Southbound");
             unsigned int new_ovnsb_cond_seqno =
                         ovsdb_idl_get_condition_seqno(ovnsb_idl_loop.idl);
             if (new_ovnsb_cond_seqno != ovnsb_cond_seqno) {
