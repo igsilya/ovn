igsilya/ovn

commit e1ab41ee32aa2782db82406b8bd9cda0e1fabc3c

controller: Update MAC binding timestamp
To achieve that add thread that will handle
statistics requests and delegate the processing
to defined functions. This allows the thread to
be flexible enough, so it could be extended in future
if needed.

At the same time connected the thread with the MAC
cache I-P node to have the timestamp updates. The
updates should happen once per dump_period
(3/4 of the aging threshold) per chassis only if
the MAC binding is actively used.

Reported-at: https://bugzilla.redhat.com/2189924
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>

tests/ovn.at
@@ -34614,6 +34614,8 @@ AT_CHECK_UNQUOTED([as hv1 ovs-ofctl dump-flows br-int table=79 --no-stats | stri
  table=79, priority=100,ip,reg14=${port_key},metadata=${dp_key},dl_src=00:00:00:00:10:20,nw_src=192.168.10.20 actions=drop
 ])
 
+timestamp=$(fetch_column mac_binding timestamp ip="192.168.10.20")
+
 send_udp hv1 ext1 10
 send_udp hv2 ext2 20
 
@@ -34621,16 +34623,27 @@ OVS_WAIT_UNTIL([as hv1 ovs-ofctl dump-flows br-int table=79 | grep "192.168.10.1
 OVS_WAIT_UNTIL([as hv2 ovs-ofctl dump-flows br-int table=79 | grep "192.168.10.20" | grep -q "n_packets=1"])
 
 # Set the MAC binding aging threshold
-AT_CHECK([ovn-nbctl set logical_router gw options:mac_binding_age_threshold=1])
-AT_CHECK([fetch_column nb:logical_router options | grep -q mac_binding_age_threshold=1])
+AT_CHECK([ovn-nbctl set logical_router gw options:mac_binding_age_threshold=5])
+AT_CHECK([fetch_column nb:logical_router options | grep -q mac_binding_age_threshold=5])
 AT_CHECK([ovn-nbctl --wait=sb sync])
 
+# Wait send few packets for "192.168.10.20" to indicate that it is still in use
+send_udp hv2 ext2 20
+sleep 1
+send_udp hv2 ext2 20
+
 # Set the timeout for OVS_WAIT* functions to 5 seconds
-OVS_CTL_TIMEOUT=5
+OVS_CTL_TIMEOUT=10
+OVS_WAIT_UNTIL([
+    test "$timestamp" != "$(fetch_column mac_binding timestamp ip='192.168.10.20')"
+])
+check $(test "$(fetch_column mac_binding timestamp ip='192.168.10.20')" != "")
+
 # Check if the records are removed after some inactivity
 OVS_WAIT_UNTIL([
     test "0" = "$(ovn-sbctl list mac_binding | grep -c '192.168.10.10')"
 ])
+# The second one takes longer because it got refreshed
 OVS_WAIT_UNTIL([
     test "0" = "$(ovn-sbctl list mac_binding | grep -c '192.168.10.20')"
 ])
@@ -35313,8 +35326,8 @@ as hv1
 check ovs-vsctl add-br br-phys
 ovn_attach n1 br-phys 192.168.0.1
 
-dnl Ensure that there are at least 3 openflow connections.
-OVS_WAIT_UNTIL([test "$(grep -c 'negotiated OpenFlow version' hv1/ovs-vswitchd.log)" -eq "3"])
+dnl Ensure that there are 4 openflow connections.
+OVS_WAIT_UNTIL([test "$(grep -c 'negotiated OpenFlow version' hv1/ovs-vswitchd.log)" -eq "4"])
 
 dnl "Wait" 3 times 60 seconds and ensure ovn-controller writes to the
 dnl openflow connections in the meantime.  This should allow ovs-vswitchd

controller/automake.mk
@@ -45,7 +45,9 @@ controller_ovn_controller_SOURCES = \
 	controller/mirror.h \
 	controller/mirror.c \
 	controller/mac_cache.h \
-	controller/mac_cache.c
+	controller/mac_cache.c \
+	controller/statctrl.h \
+	controller/statctrl.c
 
 controller_ovn_controller_LDADD = lib/libovn.la $(OVS_LIBDIR)/libopenvswitch.la
 man_MANS += controller/ovn-controller.8

controller/mac_cache.c
@@ -45,6 +45,8 @@ mac_cache_threshold_get_value_ms(const struct sbrec_datapath_binding *dp,
 static void
 mac_cache_threshold_remove(struct hmap *thresholds,
                            struct mac_cache_threshold *threshold);
+static void
+mac_cache_update_req_delay(struct hmap *thresholds, uint64_t *req_delay);
 
 bool
 mac_cache_threshold_add(struct mac_cache_data *data,
@@ -167,6 +169,84 @@ mac_cache_mac_bindings_clear(struct mac_cache_data *data)
     }
 }
 
+struct mac_cache_stats {
+    struct ovs_list list_node;
+
+    int64_t idle_age_ms;
+
+    union {
+        /* Common data to identify MAC binding. */
+        struct mac_cache_mb_data mb;
+    } data;
+};
+
+void
+mac_cache_mb_stats_process_flow_stats(struct ovs_list *stats_list,
+                                      struct ofputil_flow_stats *ofp_stats)
+{
+    struct mac_cache_stats *stats = xmalloc(sizeof *stats);
+
+    stats->idle_age_ms = ofp_stats->idle_age * 1000;
+    stats->data.mb = (struct mac_cache_mb_data) {
+        .port_key = ofp_stats->match.flow.regs[MFF_LOG_INPORT - MFF_REG0],
+        .dp_key = ntohll(ofp_stats->match.flow.metadata),
+        .mac = ofp_stats->match.flow.dl_src
+    };
+
+    if (ofp_stats->match.flow.dl_type == htons(ETH_TYPE_IP)) {
+        stats->data.mb.ip = in6_addr_mapped_ipv4(ofp_stats->match.flow.nw_src);
+    } else {
+        stats->data.mb.ip = ofp_stats->match.flow.ipv6_src;
+    }
+
+    ovs_list_push_back(stats_list, &stats->list_node);
+}
+
+void
+mac_cache_mb_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
+                       void *data)
+{
+    struct mac_cache_data *cache_data = data;
+    struct hmap *thresholds = &cache_data->thresholds[MAC_CACHE_MAC_BINDING];
+    long long timewall_now = time_wall_msec();
+
+    struct mac_cache_stats *stats;
+    LIST_FOR_EACH_POP (stats, list_node, stats_list) {
+        struct mac_cache_mac_binding *mc_mb =
+                mac_cache_mac_binding_find(cache_data, &stats->data.mb);
+        if (!mc_mb) {
+            free(stats);
+            continue;
+        }
+
+        struct uuid *dp_uuid = &mc_mb->sbrec_mb->datapath->header_.uuid;
+        struct mac_cache_threshold *threshold =
+                mac_cache_threshold_find(thresholds, dp_uuid);
+
+        /* If "idle_age" is under threshold it means that the mac binding is
+         * used on this chassis. Also make sure that we don't update the
+         * timestamp more than once during the dump period. */
+        if (stats->idle_age_ms < threshold->value &&
+            (timewall_now - mc_mb->sbrec_mb->timestamp) >=
+            threshold->dump_period) {
+            sbrec_mac_binding_set_timestamp(mc_mb->sbrec_mb, timewall_now);
+        }
+
+        free(stats);
+    }
+
+    mac_cache_update_req_delay(thresholds, req_delay);
+}
+
+void
+mac_cache_stats_destroy(struct ovs_list *stats_list)
+{
+    struct mac_cache_stats *stats;
+    LIST_FOR_EACH_POP (stats, list_node, stats_list) {
+        free(stats);
+    }
+}
+
 static uint32_t
 mac_cache_mb_data_hash(const struct mac_cache_mb_data *mb_data)
 {
@@ -267,4 +347,17 @@ mac_cache_threshold_remove(struct hmap *thresholds,
 {
     hmap_remove(thresholds, &threshold->hmap_node);
     free(threshold);
+}
+
+static void
+mac_cache_update_req_delay(struct hmap *thresholds, uint64_t *req_delay)
+{
+    struct mac_cache_threshold *threshold;
+
+    uint64_t dump_period = UINT64_MAX;
+    HMAP_FOR_EACH (threshold, hmap_node, thresholds) {
+        dump_period = MIN(dump_period, threshold->dump_period);
+    }
+
+    *req_delay = dump_period < UINT64_MAX ? dump_period : 0;
 }

controller/mac_cache.h
@@ -80,4 +80,11 @@ void mac_cache_mac_binding_remove(struct mac_cache_data *data,
 void mac_cache_mac_bindings_clear(struct mac_cache_data *data);
 bool mac_cache_sb_mac_binding_updated(const struct sbrec_mac_binding *mb);
 
+void
+mac_cache_mb_stats_process_flow_stats(struct ovs_list *stats_list,
+                                      struct ofputil_flow_stats *ofp_stats);
+void mac_cache_mb_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
+                            void *data);
+void mac_cache_stats_destroy(struct ovs_list *stats_list);
+
 #endif /* controller/mac_cache.h */

controller/ovn-controller.c
@@ -84,6 +84,7 @@
 #include "hmapx.h"
 #include "mirror.h"
 #include "mac_cache.h"
+#include "statctrl.h"
 
 VLOG_DEFINE_THIS_MODULE(main);
 
@@ -4903,6 +4904,7 @@ main(int argc, char *argv[])
     lflow_init();
     mirror_init();
     vif_plug_provider_initialize();
+    statctrl_init();
 
     /* Connect to OVS OVSDB instance. */
     struct ovsdb_idl_loop ovs_idl_loop = OVSDB_IDL_LOOP_INITIALIZER(
@@ -5248,7 +5250,7 @@ main(int argc, char *argv[])
                      runtime_data_ovs_interface_shadow_handler);
 
     engine_add_input(&en_mac_cache, &en_runtime_data,
-                     engine_noop_handler);
+                     mac_cache_runtime_data_handler);
     engine_add_input(&en_mac_cache, &en_sb_mac_binding,
                      mac_cache_sb_mac_binding_handler);
     engine_add_input(&en_mac_cache, &en_sb_datapath_binding,
@@ -5310,6 +5312,8 @@ main(int argc, char *argv[])
         engine_get_internal_data(&en_template_vars);
     struct ed_type_lb_data *lb_data =
         engine_get_internal_data(&en_lb_data);
+    struct mac_cache_data *mac_cache_data =
+            engine_get_internal_data(&en_mac_cache);
 
     ofctrl_init(&lflow_output_data->group_table,
                 &lflow_output_data->meter_table,
@@ -5698,6 +5702,11 @@ main(int argc, char *argv[])
                         }
                     }
 
+                    if (mac_cache_data) {
+                        statctrl_update(br_int->name);
+                        statctrl_run(ovnsb_idl_txn, mac_cache_data);
+                    }
+
                     ofctrl_seqno_update_create(
                         ofctrl_seq_type_nb_cfg,
                         get_nb_cfg(sbrec_sb_global_table_get(
@@ -5807,6 +5816,7 @@ main(int argc, char *argv[])
             if (br_int) {
                 ofctrl_wait();
                 pinctrl_wait(ovnsb_idl_txn);
+                statctrl_wait(ovnsb_idl_txn);
             }
 
             binding_wait();
@@ -5941,6 +5951,7 @@ loop_done:
     patch_destroy();
     mirror_destroy();
     encaps_destroy();
+    statctrl_destroy();
     if_status_mgr_destroy(if_mgr);
     shash_destroy(&vif_plug_deleted_iface_ids);
     shash_destroy(&vif_plug_changed_iface_ids);

commit 17e357204f76b1cce8c19fd93b70ab1641cd8cd1

mac-cache: Fix expiration of active MAC binding due to skipped update.
ovn-controller doesn't update timestamps until 3/4 of the expiration
threshold have passed, but then it may wait for another 3/4.  And since
3/4 + 3/4 > 1, northd would remove the entry before ovn-controller
wakes up to refresh it next time.

For example:
Let's say the threshold is 20 seconds and all the entries we create
have constant traffic flow, so should never expire.  Let's see the
timeline that starts at time A:

  1. [A]    We create one mac entry, next statistics request: A + 15.

  2. [A+5]  We create second entry, next request doesn't change and
            is still at A + 15.

  3. [A+15] We send an OF stats request and get a reply.

     a. First mac entry is updated, because dump interval is 15 and
        the original timestamp is A, which is now + some delta,
        i.e., A + 15 <= A + 15.

     b. Second entry is not updated, because (A + 5) + 15 > A + 15.

     c. The next request is scheduled for 'now + 15' (thresholds didn't
        change), so the next request is at A + 30.

  4. [A+25] northd removes the second entry since it wasn't updated and
            the threshold passed.

  5. [A+30] We send an OF stats request and get a reply, but the entry
            is already removed.

In order to fix that we must never skip the update if the next dump
period may go over the threshold.  This can be achieved by choosing the
cooldown period small enough, so together with the dump period they do
not go over the threshold and actually leave some room for all the
processing and the updates.

Let's use 1/2 of the threshold as the dump period and 1/4 as a cooldown
period.  This should fix the issue while still not sending too many
transactions to the database and not requesting the OpenFlow statistics
too often.  1/2 + 1/4 = 3/4, so we still have enough room for all the
processing before the aging threshold.

New logs added for easier debugging of such issues.

A new test is added to demonstrate the issue and verify the fix.

Reported-at: https://issues.redhat.com/browse/FDP-1130
Fixes: e1ab41ee32aa ("controller: Update MAC binding timestamp")

controller/mac-cache.c
@@ -37,6 +37,12 @@ mac_binding_data_hash(const struct mac_binding_data *mb_data);
 static inline bool
 mac_binding_data_equals(const struct mac_binding_data *a,
                         const struct mac_binding_data *b);
+static void mac_binding_update_log(const char *action,
+                                   const struct mac_binding_data *,
+                                   bool print_times,
+                                   const struct mac_cache_threshold *,
+                                   int64_t idle_age_ms,
+                                   uint64_t since_updated_ms);
 static uint32_t
 fdb_data_hash(const struct fdb_data *fdb_data);
 static inline bool
@@ -84,7 +90,16 @@ mac_cache_threshold_add(struct mac_cache_data *data,
     threshold = xmalloc(sizeof *threshold);
     threshold->dp_key = dp->tunnel_key;
     threshold->value = value;
-    threshold->dump_period = (3 * value) / 4;
+    threshold->dump_period = value / 2;
+    threshold->cooldown_period = value / 4;
+
+    /* (cooldown_period + dump_period) is the maximum time the timestamp may
+     * be not updated.  So, the sum of those times must be lower than the
+     * threshold, otherwise we may fail to update an active MAC binding in
+     * time and risk it being removed.  Giving it an extra 1/10 of the time
+     * for all the processing that needs to happen. */
+    ovs_assert(threshold->cooldown_period + threshold->dump_period
+               < (9 * value) / 10);
 
     hmap_insert(&data->thresholds, &threshold->hmap_node, dp->tunnel_key);
 }
@@ -156,10 +171,13 @@ mac_binding_add(struct hmap *map, struct mac_binding_data mb_data,
     mb->data = mb_data;
     mb->sbrec = smb;
     mb->timestamp = timestamp;
+    mac_binding_update_log("Added", &mb_data, false, NULL, 0, 0);
 }
 
 void
-mac_binding_remove(struct hmap *map, struct mac_binding *mb) {
+mac_binding_remove(struct hmap *map, struct mac_binding *mb)
+{
+    mac_binding_update_log("Removed", &mb->data, false, NULL, 0, 0);
     hmap_remove(map, &mb->hmap_node);
     free(mb);
 }
@@ -384,6 +402,33 @@ mac_binding_stats_process_flow_stats(struct ovs_list *stats_list,
     ovs_list_push_back(stats_list, &stats->list_node);
 }
 
+static void
+mac_binding_update_log(const char *action,
+                       const struct mac_binding_data *data,
+                       bool print_times,
+                       const struct mac_cache_threshold *threshold,
+                       int64_t idle_age_ms, uint64_t since_updated_ms)
+{
+    if (!VLOG_IS_DBG_ENABLED()) {
+        return;
+    }
+
+    struct ds s = DS_EMPTY_INITIALIZER;
+
+    ds_put_cstr(&s, action);
+    ds_put_cstr(&s, " MAC binding (");
+    mac_binding_data_to_string(data, &s);
+    if (print_times) {
+        ds_put_format(&s, "), last update: %"PRIu64"ms ago,"
+                          " idle age: %"PRIi64"ms, threshold: %"PRIu64"ms",
+                      since_updated_ms, idle_age_ms, threshold->value);
+    } else {
+        ds_put_char(&s, ')');
+    }
+    VLOG_DBG("%s.", ds_cstr_ro(&s));
+    ds_destroy(&s);
+}
+
 void
 mac_binding_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
                       void *data)
@@ -396,26 +441,43 @@ mac_binding_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
         struct mac_binding *mb = mac_binding_find(&cache_data->mac_bindings,
                                                   &stats->data.mb);
         if (!mb) {
+            mac_binding_update_log("Not found in the cache:", &stats->data.mb,
+                                   false, NULL, 0, 0);
             free(stats);
             continue;
         }
 
+        uint64_t since_updated_ms = timewall_now - mb->sbrec->timestamp;
         struct mac_cache_threshold *threshold =
                 mac_cache_threshold_find(cache_data, mb->data.dp_key);
 
         /* If "idle_age" is under threshold it means that the mac binding is
-         * used on this chassis. Also make sure that we don't update the
-         * timestamp more than once during the dump period. */
-        if (stats->idle_age_ms < threshold->value &&
-                (timewall_now - mb->sbrec->timestamp) >=
-            threshold->dump_period) {
-            sbrec_mac_binding_set_timestamp(mb->sbrec, timewall_now);
+         * used on this chassis. */
+        if (stats->idle_age_ms < threshold->value) {
+            if (since_updated_ms >= threshold->cooldown_period) {
+                mac_binding_update_log("Updating active", &mb->data, true,
+                                       threshold, stats->idle_age_ms,
+                                       since_updated_ms);
+                sbrec_mac_binding_set_timestamp(mb->sbrec, timewall_now);
+            } else {
+                /* Postponing the update to avoid sending database transactions
+                 * too frequently. */
+                mac_binding_update_log("Not updating active", &mb->data, true,
+                                       threshold, stats->idle_age_ms,
+                                       since_updated_ms);
+            }
+        } else {
+            mac_binding_update_log("Not updating non-active", &mb->data, true,
+                                   threshold, stats->idle_age_ms,
+                                   since_updated_ms);
         }
-
         free(stats);
     }
 
     mac_cache_update_req_delay(&cache_data->thresholds, req_delay);
+    if (*req_delay) {
+        VLOG_DBG("MAC binding statistics dalay: %"PRIu64, *req_delay);
+    }
 }
 
 /* FDB stat processing. */

controller/mac-cache.h
@@ -48,6 +48,8 @@ struct mac_cache_threshold {
     uint64_t value;
     /* Statistics dump period. */
     uint64_t dump_period;
+    /* How long to wait between two database updates. */
+    uint64_t cooldown_period;
 };
 
 struct mac_binding_data {

controller/pinctrl.c
@@ -4776,6 +4776,9 @@ mac_binding_add_to_sb(struct ovsdb_idl_txn *ovnsb_idl_txn,
         /* For backward compatibility check if timestamp column is available
          * in SB DB. */
         if (pinctrl.mac_binding_can_timestamp) {
+            VLOG_DBG("Setting MAC binding timestamp for "
+                     "ip:%s mac:%s port:%s to %lld",
+                     b->ip, b->mac, logical_port, time_wall_msec());
             sbrec_mac_binding_set_timestamp(b, time_wall_msec());
         }
     }

tests/ovn.at
@@ -35108,6 +35108,7 @@ ovs-vsctl -- add-port br-phys ext1 -- \
     options:rxq_pcap=hv1/ext1-rx.pcap \
     ofport-request=2
 ovs-vsctl set open . external_ids:ovn-bridge-mappings=physnet1:br-phys
+ovn-appctl -t ovn-controller vlog/set mac_cache:file:dbg pinctrl:file:dbg
 
 sim_add hv2
 as hv2
@@ -35125,6 +35126,7 @@ ovs-vsctl -- add-port br-phys ext2 -- \
     options:rxq_pcap=hv2/ext2-rx.pcap \
     ofport-request=2
 ovs-vsctl set open . external_ids:ovn-bridge-mappings=physnet1:br-phys
+ovn-appctl -t ovn-controller vlog/set mac_cache:file:dbg pinctrl:file:dbg
 
 OVN_POPULATE_ARP
 wait_for_ports_up
@@ -35331,6 +35333,7 @@ sim_add hv1
 as hv1
 check ovs-vsctl add-br br-phys
 ovn_attach n1 br-phys 192.168.0.1
+ovn-appctl -t ovn-controller vlog/set mac_cache:file:dbg pinctrl:file:dbg
 
 check ovn-nbctl                                                     \
     -- ls-add ls1                                                   \
@@ -35414,6 +35417,89 @@ dnl Check local mac binding cache - it should be empty now.
 AT_CHECK([as hv1 ovn-appctl -t ovn-controller debug/dump-mac-bindings], [0], [dnl
 Local MAC bindings:
 ])
+
+OVN_CLEANUP([hv1])
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([MAC binding aging - persistence of the active entry])
+AT_SKIP_IF([test $HAVE_SCAPY = no])
+ovn_start
+
+net_add n1
+sim_add hv1
+as hv1
+check ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.1
+ovn-appctl -t ovn-controller vlog/set mac_cache:file:dbg pinctrl:file:dbg
+
+check ovn-nbctl                                                     \
+    -- ls-add ls1                                                   \
+    -- ls-add ls2                                                   \
+    -- lr-add lr                                                    \
+    -- set logical_router lr options:mac_binding_age_threshold=4    \
+    -- lrp-add lr lr-ls1 00:00:00:00:10:00 192.168.10.1/24          \
+    -- lrp-add lr lr-ls2 00:00:00:00:20:00 192.168.20.1/24          \
+    -- lsp-add ls1 ls1-lr                                           \
+    -- lsp-set-type ls1-lr router                                   \
+    -- lsp-set-addresses ls1-lr router                              \
+    -- lsp-set-options ls1-lr router-port=lr-ls1                    \
+    -- lsp-add ls1 vif1                                             \
+    -- lsp-set-addresses vif1 "00:00:00:00:10:10 192.168.10.10"     \
+    -- lsp-add ls2 ls2-lr                                           \
+    -- lsp-set-type ls2-lr router                                   \
+    -- lsp-set-addresses ls2-lr router                              \
+    -- lsp-set-options ls2-lr router-port=lr-ls2                    \
+    -- lsp-add ls2 vif2                                             \
+    -- lsp-set-addresses vif2 "00:00:00:00:20:10 192.168.20.10"
+
+check ovs-vsctl                                      \
+    -- add-port br-int vif1                          \
+    -- set interface vif1 external-ids:iface-id=vif1 \
+    -- add-port br-int vif2                          \
+    -- set interface vif2 external-ids:iface-id=vif2
+
+OVN_POPULATE_ARP
+wait_for_ports_up
+check ovn-nbctl --wait=hv sync
+
+dnl Wait for pinctrl thread to be connected.
+OVS_WAIT_UNTIL([grep pinctrl hv1/ovn-controller.log | grep -q connected])
+
+dnl Populate MAC Binding entries.  Wait between two, so they have an initial
+dnl difference in timestamps around half the age threshold.
+send_garp hv1 vif2 2 00:00:00:00:20:10 ff:ff:ff:ff:ff:ff 192.168.20.10 192.168.20.10
+wait_row_count mac_binding 1 ip="192.168.20.10" logical_port="lr-ls2"
+sleep 2
+send_garp hv1 vif1 2 00:00:00:00:10:10 ff:ff:ff:ff:ff:ff 192.168.10.10 192.168.10.10
+wait_row_count mac_binding 1 ip="192.168.10.10" logical_port="lr-ls1"
+
+send_udp() {
+    local hv=$1 dev=$2 byte=$3
+
+    packet=$(fmt_pkt "Ether(dst='00:00:00:00:10:00', src='00:00:00:00:10:${byte}')/ \
+                      IP(dst='192.168.20.${byte}', src='192.168.10.${byte}')/UDP()")
+    check as $hv ovs-appctl netdev-dummy/receive $dev $packet
+}
+
+uuid=$(fetch_column mac_binding _uuid ip="192.168.10.10" logical_port="lr-ls1")
+for i in $(seq 12); do
+    # Keep one entry alive by sending traffic that uses it.
+    send_udp hv1 vif1 10
+    sleep 1
+    # The entry must not expire.
+    check_row_count mac_binding 1 ip="192.168.10.10" logical_port="lr-ls1"
+    as hv1 ovs-ofctl dump-flows br-int table=OFTABLE_MAC_CACHE_USE
+done
+# Check that it's the same entry.
+uuid2=$(fetch_column mac_binding _uuid ip="192.168.10.10" logical_port="lr-ls1")
+check test "$uuid" = "$uuid"
+
+# The other entry must have expired by now.
+check_row_count mac_binding 0 ip="192.168.20.10" logical_port="lr-ls2"
+# Wait for the alive one to expire as well.
+wait_row_count mac_binding 0 ip="192.168.10.10" logical_port="lr-ls1"
 
 OVN_CLEANUP([hv1])
 AT_CLEANUP