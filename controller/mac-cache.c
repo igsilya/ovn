igsilya/ovn

commit a68ac1a4a1ea17a72ec03956ec20bfeb668a3acf

mac-cache: Properly handle deletion of SB mac_bindings.
SB.MAC_Binding doesn't store a strong reference to SB.Port_Binding.
Instead it stores the port binding 'logical_port' name.  As the
mac-binding cache in ovn-controller stored entries indexed by
[datapath-key, port-key, ip] it meant that when processing
SB.MAC_Binding changes (including deletion) it had to first lookup the
corresponding SB.Port_Binding.

If the logical port associated with the MAC binding is deleted in the
same transaction then looking it up in the IDL index fails (deleted IDL
records are not indexed).  This leads to stale entries staying in the
mac cache and in consequence to potential use after free (of the SB
MAC_Binding record) or to crashes if the statctrl module tries to update
timestamps of MAC_Bindings that happened to have been removed in the
current iteration (but still linger in the cache).

In practice we don't need the port-key we can simply use the port
binding UUID as unique index.  However, the same struct mac_binding type
is used by pinctrl to temporarily efficiently store buffered packets for
yet to be resolved MAC bindings.  In those cases there's no SB
MAC_Binding record yet so the logical ingress port tunnel_key is used as
index.

The two ways of using the MAC binding maps are orthogonal, that is, we
will never use a single map to store both types of bindings.

In order to avoid issues we now include the SB MAC_Binding UUID into the
key of the struct mac_binding_data.

Reported-at: https://issues.redhat.com/browse/FDP-906
Fixes: b57f60a617b4 ("controller: Add MAC cache I-P node")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Acked-by: Mark Michelson <mmichels@redhat.com>
Acked-by: Ales Musil <amusil@redhat.com>

controller/mac-cache.c
@@ -142,21 +142,18 @@ mac_cache_thresholds_clear(struct mac_cache_data *data)
 }
 
 /* MAC binding. */
-struct mac_binding *
+void
 mac_binding_add(struct hmap *map, struct mac_binding_data mb_data,
                 long long timestamp) {
 
     struct mac_binding *mb = mac_binding_find(map, &mb_data);
     if (!mb) {
         mb = xmalloc(sizeof *mb);
-        mb->sbrec_mb = NULL;
         hmap_insert(map, &mb->hmap_node, mac_binding_data_hash(&mb_data));
     }
 
     mb->data = mb_data;
     mb->timestamp = timestamp;
-
-    return mb;
 }
 
 void
@@ -181,24 +178,37 @@ mac_binding_find(const struct hmap *map,
 }
 
 bool
-mac_binding_data_from_sbrec(struct mac_binding_data *data,
-                            const struct sbrec_mac_binding *mb,
-                            struct ovsdb_idl_index *sbrec_pb_by_name)
+mac_binding_data_parse(struct mac_binding_data *data,
+                       uint32_t dp_key, uint32_t port_key,
+                       const char *ip_str, const char *mac_str)
 {
-    const struct sbrec_port_binding *pb =
-            lport_lookup_by_name(sbrec_pb_by_name, mb->logical_port);
+    struct eth_addr mac;
+    struct in6_addr ip;
 
-    if (!pb || !pb->datapath || !ip46_parse(mb->ip, &data->ip) ||
-        !eth_addr_from_string(mb->mac, &data->mac)) {
+    if (!ip46_parse(ip_str, &ip) || !eth_addr_from_string(mac_str, &mac)) {
         static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
-        VLOG_WARN_RL(&rl, "Couldn't parse MAC binding: ip=%s, mac=%s, "
-                     "logical_port=%s", mb->ip, mb->mac, mb->logical_port);
+        VLOG_WARN_RL(&rl, "Couldn't parse MAC binding: ip=%s, mac=%s",
+                     ip_str, mac_str);
         return false;
     }
 
-    data->dp_key = mb->datapath->tunnel_key;
-    data->port_key = pb->tunnel_key;
+    mac_binding_data_init(data, dp_key, port_key, ip, mac);
+    return true;
+}
 
+bool
+mac_binding_data_from_sbrec(struct mac_binding_data *data,
+                            const struct sbrec_mac_binding *mb)
+{
+    /* This explicitly sets the port_key to 0 as port_binding tunnel_keys
+     * can change.  Instead use add the SB.MAC_Binding UUID as key; this
+     * makes the mac_binding_data key unique. */
+    if (!mac_binding_data_parse(data, mb->datapath->tunnel_key, 0,
+                                mb->ip, mb->mac)) {
+        return false;
+    }
+
+    data->sbrec = mb;
     return true;
 }
 
@@ -208,6 +218,30 @@ mac_bindings_clear(struct hmap *map)
     struct mac_binding *mb;
     HMAP_FOR_EACH_POP (mb, hmap_node, map) {
         free(mb);
+    }
+}
+
+void
+mac_bindings_to_string(const struct hmap *map, struct ds *out_data)
+{
+    struct mac_binding *mb;
+    HMAP_FOR_EACH (mb, hmap_node, map) {
+        char ip[INET6_ADDRSTRLEN];
+
+        if (!ipv6_string_mapped(ip, &mb->data.ip)) {
+            continue;
+        }
+        if (mb->data.sbrec) {
+            ds_put_format(out_data, "SB UUID: " UUID_FMT ", ",
+                          UUID_ARGS(&mb->data.sbrec->header_.uuid));
+        } else {
+            ds_put_cstr(out_data, "SB UUID: <none>, ");
+        }
+        ds_put_format(out_data, "datapath-key: %"PRIu32", "
+                                "port-key: %"PRIu32", "
+                                "ip: %s, mac: " ETH_ADDR_FMT "\n",
+                      mb->data.dp_key, mb->data.port_key,
+                      ip, ETH_ADDR_ARGS(mb->data.mac));
     }
 }
 
@@ -382,9 +416,9 @@ mac_binding_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
          * used on this chassis. Also make sure that we don't update the
          * timestamp more than once during the dump period. */
         if (stats->idle_age_ms < threshold->value &&
-                (timewall_now - mb->sbrec_mb->timestamp) >=
+                (timewall_now - mb->data.sbrec->timestamp) >=
             threshold->dump_period) {
-            sbrec_mac_binding_set_timestamp(mb->sbrec_mb, timewall_now);
+            sbrec_mac_binding_set_timestamp(mb->data.sbrec, timewall_now);
         }
 
         free(stats);
@@ -608,7 +642,9 @@ mac_cache_stats_destroy(struct ovs_list *stats_list)
 static uint32_t
 mac_binding_data_hash(const struct mac_binding_data *mb_data)
 {
-    uint32_t hash = 0;
+    uint32_t hash = mb_data->sbrec
+                    ? uuid_hash(&mb_data->sbrec->header_.uuid)
+                    : 0;
 
     hash = hash_add(hash, mb_data->port_key);
     hash = hash_add(hash, mb_data->dp_key);
@@ -621,9 +657,10 @@ static inline bool
 mac_binding_data_equals(const struct mac_binding_data *a,
                         const struct mac_binding_data *b)
 {
-    return a->port_key == b->port_key &&
+    return a->sbrec == b->sbrec &&
+           a->port_key == b->port_key &&
            a->dp_key == b->dp_key &&
-            ipv6_addr_equals(&a->ip, &b->ip);
+           ipv6_addr_equals(&a->ip, &b->ip);
 }
 
 static uint32_t

controller/mac-cache.h
@@ -52,6 +52,7 @@ struct mac_cache_threshold {
 
 struct mac_binding_data {
     /* Keys. */
+    const struct sbrec_mac_binding *sbrec; /* Only the UUID is used as key.*/
     uint32_t port_key;
     uint32_t dp_key;
     struct in6_addr ip;
@@ -63,8 +64,6 @@ struct mac_binding {
     struct hmap_node hmap_node;
     /* Common data to identify MAC binding. */
     struct mac_binding_data data;
-    /* Reference to the SB MAC binding record (Might be NULL). */
-    const struct sbrec_mac_binding *sbrec_mb;
     /* User specified timestamp (in ms) */
     long long timestamp;
 };
@@ -128,20 +127,37 @@ void mac_cache_thresholds_sync(struct mac_cache_data *data,
 void mac_cache_thresholds_clear(struct mac_cache_data *data);
 
 /* MAC binding. */
-struct mac_binding *mac_binding_add(struct hmap *map,
-                                    struct mac_binding_data mb_data,
-                                    long long timestamp);
+
+static inline void
+mac_binding_data_init(struct mac_binding_data *data,
+                      uint32_t dp_key, uint32_t port_key,
+                      struct in6_addr ip, struct eth_addr mac)
+{
+    *data = (struct mac_binding_data) {
+        .sbrec = NULL,
+        .dp_key = (dp_key),
+        .port_key = (port_key),
+        .ip = (ip),
+        .mac = (mac),
+    };
+}
+
+void mac_binding_add(struct hmap *map, struct mac_binding_data mb_data,
+                     long long timestamp);
 
 void mac_binding_remove(struct hmap *map, struct mac_binding *mb);
 
 struct mac_binding *mac_binding_find(const struct hmap *map,
                                      const struct mac_binding_data *mb_data);
 
+bool mac_binding_data_parse(struct mac_binding_data *data,
+                            uint32_t dp_key, uint32_t port_key,
+                            const char *ip_str, const char *mac_str);
 bool mac_binding_data_from_sbrec(struct mac_binding_data *data,
-                                 const struct sbrec_mac_binding *mb,
-                                 struct ovsdb_idl_index *sbrec_pb_by_name);
+                                 const struct sbrec_mac_binding *mb);
 
 void mac_bindings_clear(struct hmap *map);
+void mac_bindings_to_string(const struct hmap *map, struct ds *out_data);
 
 bool sb_mac_binding_updated(const struct sbrec_mac_binding *mb);
 

controller/ovn-controller.c
@@ -101,6 +101,7 @@ static unixctl_cb_func debug_status_execution;
 static unixctl_cb_func debug_dump_local_bindings;
 static unixctl_cb_func debug_dump_related_lports;
 static unixctl_cb_func debug_dump_local_template_vars;
+static unixctl_cb_func debug_dump_local_mac_bindings;
 static unixctl_cb_func debug_dump_lflow_conj_ids;
 static unixctl_cb_func lflow_cache_flush_cmd;
 static unixctl_cb_func lflow_cache_show_stats_cmd;
@@ -2932,26 +2933,22 @@ en_lb_data_cleanup(void *data)
 
 static void
 mac_binding_add_sb(struct mac_cache_data *data,
-                   const struct sbrec_mac_binding *smb,
-                   struct ovsdb_idl_index *sbrec_pb_by_name)
+                   const struct sbrec_mac_binding *smb)
 {
     struct mac_binding_data mb_data;
-    if (!mac_binding_data_from_sbrec(&mb_data, smb, sbrec_pb_by_name)) {
+    if (!mac_binding_data_from_sbrec(&mb_data, smb)) {
         return;
     }
 
-    struct mac_binding *mb = mac_binding_add(&data->mac_bindings, mb_data, 0);
-
-    mb->sbrec_mb = smb;
+    mac_binding_add(&data->mac_bindings, mb_data, 0);
 }
 
 static void
 mac_binding_remove_sb(struct mac_cache_data *data,
-                      const struct sbrec_mac_binding *smb,
-                      struct ovsdb_idl_index *sbrec_pb_by_name)
+                      const struct sbrec_mac_binding *smb)
 {
     struct mac_binding_data mb_data;
-    if (!mac_binding_data_from_sbrec(&mb_data, smb, sbrec_pb_by_name)) {
+    if (!mac_binding_data_from_sbrec(&mb_data, smb)) {
         return;
     }
 
@@ -2995,8 +2992,7 @@ fdb_remove_sb(struct mac_cache_data *data, const struct sbrec_fdb *sfdb)
 static void
 mac_cache_mb_handle_for_datapath(struct mac_cache_data *data,
                                  const struct sbrec_datapath_binding *dp,
-                                 struct ovsdb_idl_index *sbrec_mb_by_dp,
-                                 struct ovsdb_idl_index *sbrec_pb_by_name)
+                                 struct ovsdb_idl_index *sbrec_mb_by_dp)
 {
     bool has_threshold = mac_cache_threshold_find(data, dp->tunnel_key);
 
@@ -3007,9 +3003,9 @@ mac_cache_mb_handle_for_datapath(struct mac_cache_data *data,
     const struct sbrec_mac_binding *mb;
     SBREC_MAC_BINDING_FOR_EACH_EQUAL (mb, mb_index_row, sbrec_mb_by_dp) {
         if (has_threshold) {
-            mac_binding_add_sb(data, mb, sbrec_pb_by_name);
+            mac_binding_add_sb(data, mb);
         } else {
-            mac_binding_remove_sb(data, mb, sbrec_pb_by_name);
+            mac_binding_remove_sb(data, mb);
         }
     }
 
@@ -3064,10 +3060,6 @@ en_mac_cache_run(struct engine_node *node, void *data)
             engine_ovsdb_node_get_index(
                     engine_get_input("SB_mac_binding", node),
                     "datapath");
-    struct ovsdb_idl_index *sbrec_pb_by_name =
-            engine_ovsdb_node_get_index(
-                    engine_get_input("SB_port_binding", node),
-                    "name");
     struct ovsdb_idl_index *sbrec_fdb_by_dp_key =
             engine_ovsdb_node_get_index(
                     engine_get_input("SB_fdb", node),
@@ -3086,7 +3078,7 @@ en_mac_cache_run(struct engine_node *node, void *data)
 
         mac_cache_threshold_add(cache_data, sbrec_dp);
         mac_cache_mb_handle_for_datapath(cache_data, sbrec_dp,
-                                         sbrec_mb_by_dp, sbrec_pb_by_name);
+                                         sbrec_mb_by_dp);
         mac_cache_fdb_handle_for_datapath(cache_data, sbrec_dp,
                                           sbrec_fdb_by_dp_key);
     }
@@ -3102,11 +3094,6 @@ mac_cache_sb_mac_binding_handler(struct engine_node *node, void *data)
             engine_get_input_data("runtime_data", node);
     const struct sbrec_mac_binding_table *mb_table =
             EN_OVSDB_GET(engine_get_input("SB_mac_binding", node));
-    struct ovsdb_idl_index *sbrec_pb_by_name =
-            engine_ovsdb_node_get_index(
-                    engine_get_input("SB_port_binding", node),
-                    "name");
-
     size_t previous_size = hmap_count(&cache_data->mac_bindings);
 
     const struct sbrec_mac_binding *sbrec_mb;
@@ -3116,8 +3103,7 @@ mac_cache_sb_mac_binding_handler(struct engine_node *node, void *data)
         }
 
         if (!sbrec_mac_binding_is_new(sbrec_mb)) {
-            mac_binding_remove_sb(cache_data, sbrec_mb,
-                                  sbrec_pb_by_name);
+            mac_binding_remove_sb(cache_data, sbrec_mb);
         }
 
         if (sbrec_mac_binding_is_deleted(sbrec_mb) ||
@@ -3128,7 +3114,7 @@ mac_cache_sb_mac_binding_handler(struct engine_node *node, void *data)
 
         if (mac_cache_threshold_find(cache_data,
                                      sbrec_mb->datapath->tunnel_key)) {
-            mac_binding_add_sb(cache_data, sbrec_mb, sbrec_pb_by_name);
+            mac_binding_add_sb(cache_data, sbrec_mb);
         }
     }
 
@@ -3189,10 +3175,6 @@ mac_cache_runtime_data_handler(struct engine_node *node, void *data OVS_UNUSED)
             engine_ovsdb_node_get_index(
                     engine_get_input("SB_mac_binding", node),
                     "datapath");
-    struct ovsdb_idl_index *sbrec_pb_by_name =
-            engine_ovsdb_node_get_index(
-                    engine_get_input("SB_port_binding", node),
-                    "name");
     struct ovsdb_idl_index *sbrec_fdb_by_dp_key =
             engine_ovsdb_node_get_index(
                     engine_get_input("SB_fdb", node),
@@ -3217,7 +3199,7 @@ mac_cache_runtime_data_handler(struct engine_node *node, void *data OVS_UNUSED)
 
     HMAP_FOR_EACH (tdp, node, &rt_data->tracked_dp_bindings) {
         mac_cache_mb_handle_for_datapath(cache_data, tdp->dp,
-                                         sbrec_mb_by_dp, sbrec_pb_by_name);
+                                         sbrec_mb_by_dp);
 
         mac_cache_fdb_handle_for_datapath(cache_data, tdp->dp,
                                           sbrec_fdb_by_dp_key);
@@ -3243,10 +3225,6 @@ mac_cache_sb_datapath_binding_handler(struct engine_node *node, void *data)
             engine_ovsdb_node_get_index(
                     engine_get_input("SB_mac_binding", node),
                     "datapath");
-    struct ovsdb_idl_index *sbrec_pb_by_name =
-            engine_ovsdb_node_get_index(
-                    engine_get_input("SB_port_binding", node),
-                    "name");
     struct ovsdb_idl_index *sbrec_fdb_by_dp_key =
             engine_ovsdb_node_get_index(
                     engine_get_input("SB_fdb", node),
@@ -3274,7 +3252,7 @@ mac_cache_sb_datapath_binding_handler(struct engine_node *node, void *data)
 
     SBREC_DATAPATH_BINDING_TABLE_FOR_EACH_TRACKED (sbrec_dp, dp_table) {
         mac_cache_mb_handle_for_datapath(cache_data, sbrec_dp,
-                                         sbrec_mb_by_dp, sbrec_pb_by_name);
+                                         sbrec_mb_by_dp);
 
         mac_cache_fdb_handle_for_datapath(cache_data, sbrec_dp,
                                           sbrec_fdb_by_dp_key);
@@ -5399,6 +5377,10 @@ main(int argc, char *argv[])
                              debug_dump_local_template_vars,
                              &template_vars_data->local_templates);
 
+    unixctl_command_register("debug/dump-mac-bindings", "", 0, 0,
+                             debug_dump_local_mac_bindings,
+                             &mac_cache_data->mac_bindings);
+
     unixctl_command_register("debug/ignore-startup-delay", "", 0, 0,
                              debug_ignore_startup_delay, NULL);
 
@@ -6354,6 +6336,19 @@ debug_dump_local_template_vars(struct unixctl_conn *conn, int argc OVS_UNUSED,
     local_templates_to_string(local_vars, &tv_str);
     unixctl_command_reply(conn, ds_cstr(&tv_str));
     ds_destroy(&tv_str);
+}
+
+static void
+debug_dump_local_mac_bindings(struct unixctl_conn *conn, int argc OVS_UNUSED,
+                               const char *argv[] OVS_UNUSED,
+                               void *mac_bindings)
+{
+    struct ds mb_str = DS_EMPTY_INITIALIZER;
+
+    ds_put_cstr(&mb_str, "Local MAC bindings:\n");
+    mac_bindings_to_string(mac_bindings, &mb_str);
+    unixctl_command_reply(conn, ds_cstr(&mb_str));
+    ds_destroy(&mb_str);
 }
 
 static void

commit d11f3bc01b69d3f4ada0a75c7e299db287e5b6a2

mac-cache: Fix MAC binding entry lookup for timestamp refresh.
MAC binding entry lookups are broken in two ways:

1. Port key is explicitly set to zero while creating entries from Sb
   records, but it is populated from the OpenFlow statistics.
2. Sb record pointer is populated while creating entries from Sb
   records, but it is not available while constructing MAC binding
   data from OpenFlow statistics replies.

Since both the port key and the Sb UUID are parts of the hash and the
equality check, it's actually impossible to find MAC binding cache
entries from OpenFlow statistics.

Fix that by not populating port keys from OpenFlow and using a cookie
instead of Sb UUID.

This set of information doesn't guarantee uniqueness, unfortunately,
but we can't actually get it if we don't have a full index to use.
To reduce chances of collision we could extend the cookie in the
future to 64 bits or store the full Sb UUID in the action notes in OF.
But it seems excessive at the moment, as we need to at least bring
the timestamp update to a working condition.  Having a cookie plus the
IP address and the datapath key should be a good enough distinguishing
factor.

The issue wasn't caught by the existing test because it waits for
timestamps to change without checking if they actually changing in
the right way.  There are two timestamps in the string and we wait
for the string to change and then checking that it's not empty.
What happens is that one entry expires and we're left with only one
timestamp, which is the same as before.  But the string changed and
it's not empty, so the check passes.

The printing function is split instead of just being modified. A
part of it will be used in the next commits, so splitting here to
avoid touching it twice.

Reported-at: https://issues.redhat.com/browse/FDP-1131
Fixes: a68ac1a4a1ea ("mac-cache: Properly handle deletion of SB mac_bindings.")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

controller/mac-cache.c
@@ -145,8 +145,8 @@ mac_cache_thresholds_clear(struct mac_cache_data *data)
 /* MAC binding. */
 void
 mac_binding_add(struct hmap *map, struct mac_binding_data mb_data,
-                long long timestamp) {
-
+                const struct sbrec_mac_binding *smb, long long timestamp)
+{
     struct mac_binding *mb = mac_binding_find(map, &mb_data);
     if (!mb) {
         mb = xmalloc(sizeof *mb);
@@ -154,6 +154,7 @@ mac_binding_add(struct hmap *map, struct mac_binding_data mb_data,
     }
 
     mb->data = mb_data;
+    mb->sbrec = smb;
     mb->timestamp = timestamp;
 }
 
@@ -209,7 +210,7 @@ mac_binding_data_from_sbrec(struct mac_binding_data *data,
         return false;
     }
 
-    data->sbrec = mb;
+    data->cookie = mb->header_.uuid.parts[0];
     return true;
 }
 
@@ -222,27 +223,30 @@ mac_bindings_clear(struct hmap *map)
     }
 }
 
+static void
+mac_binding_data_to_string(const struct mac_binding_data *data,
+                           struct ds *out_data)
+{
+    char ip[INET6_ADDRSTRLEN];
+
+    if (!ipv6_string_mapped(ip, &data->ip)) {
+        return;
+    }
+    ds_put_format(out_data, "cookie: 0x%08"PRIx64", "
+                            "datapath-key: %"PRIu32", "
+                            "port-key: %"PRIu32", "
+                            "ip: %s, mac: " ETH_ADDR_FMT,
+                  data->cookie, data->dp_key, data->port_key,
+                  ip, ETH_ADDR_ARGS(data->mac));
+}
+
 void
 mac_bindings_to_string(const struct hmap *map, struct ds *out_data)
 {
     struct mac_binding *mb;
     HMAP_FOR_EACH (mb, hmap_node, map) {
-        char ip[INET6_ADDRSTRLEN];
-
-        if (!ipv6_string_mapped(ip, &mb->data.ip)) {
-            continue;
-        }
-        if (mb->data.sbrec) {
-            ds_put_format(out_data, "SB UUID: " UUID_FMT ", ",
-                          UUID_ARGS(&mb->data.sbrec->header_.uuid));
-        } else {
-            ds_put_cstr(out_data, "SB UUID: <none>, ");
-        }
-        ds_put_format(out_data, "datapath-key: %"PRIu32", "
-                                "port-key: %"PRIu32", "
-                                "ip: %s, mac: " ETH_ADDR_FMT "\n",
-                      mb->data.dp_key, mb->data.port_key,
-                      ip, ETH_ADDR_ARGS(mb->data.mac));
+        mac_binding_data_to_string(&mb->data, out_data);
+        ds_put_char(out_data, '\n');
     }
 }
 
@@ -364,7 +368,9 @@ mac_binding_stats_process_flow_stats(struct ovs_list *stats_list,
 
     stats->idle_age_ms = ofp_stats->idle_age * 1000;
     stats->data.mb = (struct mac_binding_data) {
-        .port_key = ofp_stats->match.flow.regs[MFF_LOG_INPORT - MFF_REG0],
+        .cookie = ntohll(ofp_stats->cookie),
+        /* The port_key must be zero to match mac_binding_data_from_sbrec. */
+        .port_key = 0,
         .dp_key = ntohll(ofp_stats->match.flow.metadata),
         .mac = ofp_stats->match.flow.dl_src
     };
@@ -401,9 +407,9 @@ mac_binding_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
          * used on this chassis. Also make sure that we don't update the
          * timestamp more than once during the dump period. */
         if (stats->idle_age_ms < threshold->value &&
-                (timewall_now - mb->data.sbrec->timestamp) >=
+                (timewall_now - mb->sbrec->timestamp) >=
             threshold->dump_period) {
-            sbrec_mac_binding_set_timestamp(mb->data.sbrec, timewall_now);
+            sbrec_mac_binding_set_timestamp(mb->sbrec, timewall_now);
         }
 
         free(stats);
@@ -627,9 +633,7 @@ mac_cache_stats_destroy(struct ovs_list *stats_list)
 static uint32_t
 mac_binding_data_hash(const struct mac_binding_data *mb_data)
 {
-    uint32_t hash = mb_data->sbrec
-                    ? uuid_hash(&mb_data->sbrec->header_.uuid)
-                    : 0;
+    uint32_t hash = hash_uint64(mb_data->cookie);
 
     hash = hash_add(hash, mb_data->port_key);
     hash = hash_add(hash, mb_data->dp_key);
@@ -642,7 +646,7 @@ static inline bool
 mac_binding_data_equals(const struct mac_binding_data *a,
                         const struct mac_binding_data *b)
 {
-    return a->sbrec == b->sbrec &&
+    return a->cookie == b->cookie &&
            a->port_key == b->port_key &&
            a->dp_key == b->dp_key &&
            ipv6_addr_equals(&a->ip, &b->ip);

controller/mac-cache.h
@@ -52,7 +52,7 @@ struct mac_cache_threshold {
 
 struct mac_binding_data {
     /* Keys. */
-    const struct sbrec_mac_binding *sbrec; /* Only the UUID is used as key.*/
+    uint64_t cookie;
     uint32_t port_key;
     uint32_t dp_key;
     struct in6_addr ip;
@@ -64,6 +64,8 @@ struct mac_binding {
     struct hmap_node hmap_node;
     /* Common data to identify MAC binding. */
     struct mac_binding_data data;
+    /* Reference to the SB MAC binding record (Might be NULL). */
+    const struct sbrec_mac_binding *sbrec;
     /* User specified timestamp (in ms) */
     long long timestamp;
 };
@@ -134,7 +136,7 @@ mac_binding_data_init(struct mac_binding_data *data,
                       struct in6_addr ip, struct eth_addr mac)
 {
     *data = (struct mac_binding_data) {
-        .sbrec = NULL,
+        .cookie = 0,
         .dp_key = (dp_key),
         .port_key = (port_key),
         .ip = (ip),
@@ -143,7 +145,7 @@ mac_binding_data_init(struct mac_binding_data *data,
 }
 
 void mac_binding_add(struct hmap *map, struct mac_binding_data mb_data,
-                     long long timestamp);
+                     const struct sbrec_mac_binding *, long long timestamp);
 
 void mac_binding_remove(struct hmap *map, struct mac_binding *mb);
 

controller/ovn-controller.c
@@ -3025,7 +3025,7 @@ mac_binding_add_sb(struct mac_cache_data *data,
         return;
     }
 
-    mac_binding_add(&data->mac_bindings, mb_data, 0);
+    mac_binding_add(&data->mac_bindings, mb_data, smb, 0);
 }
 
 static void

controller/pinctrl.c
@@ -4717,7 +4717,7 @@ pinctrl_handle_put_mac_binding(const struct flow *md,
                      ? random_range(MAX_MAC_BINDING_DELAY_MSEC) + 1
                      : 0;
     long long timestamp = time_msec() + delay;
-    mac_binding_add(&put_mac_bindings, mb_data, timestamp);
+    mac_binding_add(&put_mac_bindings, mb_data, NULL, timestamp);
 
     /* We can send the buffered packet once the main ovn-controller
      * thread calls pinctrl_run() and it writes the mac_bindings stored
@@ -4920,7 +4920,7 @@ run_buffered_binding(const struct sbrec_mac_binding_table *mac_binding_table,
             continue;
         }
 
-        mac_binding_add(&recent_mbs, mb_data, 0);
+        mac_binding_add(&recent_mbs, mb_data, smb, 0);
 
         const char *redirect_port =
             smap_get(&pb->options, "chassis-redirect-port");
@@ -4937,7 +4937,7 @@ run_buffered_binding(const struct sbrec_mac_binding_table *mac_binding_table,
         /* Add the same entry also for chassisredirect port as the buffered
          * traffic might be buffered on the cr port. */
         mb_data.port_key = pb->tunnel_key;
-        mac_binding_add(&recent_mbs, mb_data, 0);
+        mac_binding_add(&recent_mbs, mb_data, smb, 0);
     }
 
     buffered_packets_ctx_run(&buffered_packets_ctx, &recent_mbs,

tests/ovn.at
@@ -35170,6 +35170,7 @@ $sorted_table
 ])
 
 timestamp=$(fetch_column mac_binding timestamp ip="192.168.10.20")
+binding_uuid=$(fetch_column mac_binding _uuid ip="192.168.10.20")
 
 send_udp hv1 ext1 10
 send_udp hv2 ext2 20
@@ -35192,7 +35193,10 @@ OVS_CTL_TIMEOUT=10
 OVS_WAIT_UNTIL([
     test "$timestamp" != "$(fetch_column mac_binding timestamp ip='192.168.10.20')"
 ])
-check test "$(fetch_column mac_binding timestamp ip='192.168.10.20')" != ""
+# Should still have 2 entries, i.e. none should expire yet.
+check_row_count mac_binding 2 ip='192.168.10.20'
+# Must be the same updated entries, not re-created.
+check test "$binding_uuid" = "$(fetch_column mac_binding _uuid ip='192.168.10.20')"
 
 # Check if the records are removed after some inactivity for gw-1. Only 1 entry should be present for gw-2.
 OVS_WAIT_UNTIL([
@@ -35368,10 +35372,10 @@ wait_row_count mac_binding 1 ip="192.168.10.10" logical_port="lr-ls1"
 
 dnl Check local mac binding cache.
 lr_key=$(fetch_column datapath_binding tunnel_key external_ids:name=lr)
-mb_uuid=$(fetch_column mac_binding _uuid logical_port=lr-ls1)
+cookie=$(fetch_column mac_binding _uuid logical_port=lr-ls1 | cut -d'-' -f1)
 OVS_WAIT_FOR_OUTPUT_UNQUOTED([as hv1 ovn-appctl -t ovn-controller debug/dump-mac-bindings], [0], [dnl
 Local MAC bindings:
-SB UUID: $mb_uuid, datapath-key: $lr_key, port-key: 0, ip: 192.168.10.10, mac: 00:00:00:00:10:10
+cookie: 0x$cookie, datapath-key: $lr_key, port-key: 0, ip: 192.168.10.10, mac: 00:00:00:00:10:10
 ])
 
 dnl Remove router port.  This deletes the mac binding in the same transaction.
@@ -35394,10 +35398,10 @@ send_garp hv1 vif2 2 00:00:00:00:20:10 ff:ff:ff:ff:ff:ff 192.168.20.10 192.168.2
 wait_row_count mac_binding 1 ip="192.168.20.10" logical_port="lr-ls2"
 
 dnl Check local mac binding cache.
-mb_uuid=$(fetch_column mac_binding _uuid logical_port=lr-ls2)
+cookie=$(fetch_column mac_binding _uuid logical_port=lr-ls2 | cut -d'-' -f1)
 OVS_WAIT_FOR_OUTPUT_UNQUOTED([as hv1 ovn-appctl -t ovn-controller debug/dump-mac-bindings], [0], [dnl
 Local MAC bindings:
-SB UUID: $mb_uuid, datapath-key: $lr_key, port-key: 0, ip: 192.168.20.10, mac: 00:00:00:00:20:10
+cookie: 0x$cookie, datapath-key: $lr_key, port-key: 0, ip: 192.168.20.10, mac: 00:00:00:00:20:10
 ])
 
 dnl Change LRP tunnel key.